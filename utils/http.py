f1f26faa4bc1363e12cbec97148c9978adedad06
D3SOX <nico@d3sox.me>
2025-10-24 22:51:52 +0200

    feat: enhance HTTP GET function to support custom headers

--- utils/http.py
+++ utils/http.py
@@ -1,9 +1,9 @@
 import aiohttp
-from typing import Literal
+from typing import Literal, Optional, Dict, Any
 
-async def get(url, res_method: Literal["read", "json"]):
+async def get(url, res_method: Literal["read", "json"], headers: Optional[Dict[str, str]] = None):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(url, headers=headers) as response:
             if res_method == "read":
                 return await response.read()
             elif res_method == "json":
